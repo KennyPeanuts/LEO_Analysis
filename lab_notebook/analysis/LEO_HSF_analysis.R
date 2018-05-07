5a4c1edddd53fb7d08a3014b6b67e46225430af0
Kenneth Fortino <fortinok@longwood.edu>
2018-05-07 15:49:22 -0400

    updated code for Beazley poster

--- lab_notebook/analysis/LEO_HSF_analysis.R
+++ lab_notebook/analysis/LEO_HSF_analysis.R
@@ -14,16 +14,6 @@ pres$Date.Time <- as.POSIXct(pres$Date.Time)
 
 
 # Data Analysis
-## Determine Range and Variation of Parameters
-
-    hist(YSI$ODOSAT)
-    hist(YSI$ODO)
-    hist(YSI$TURB)
-    hist(YSI$TEMP)
-    hist(YSI$SAL)
-    hist(YSI$FDOM)
-    hist(YSI$PH)
-    
 ### Analyze by Time
 # Create proxy numeirc variable for time for the YSI data frame
 time_point <- 1:length(YSI$Date) # Date used but any variable will work
@@ -32,64 +22,80 @@ time_point <- 1:length(YSI$Date) # Date used but any variable will work
 plot(ODO ~ time_point, data = YSI, subset = Date >= "2017-11-01" & Date <= "2017-11-01" , type= "p", ylab = "Dissolved Oxygen (mg/L)", main = "2017-11-01" , col= "blue", pch = 1, ylim = c(5, 15))
 
 
-#### Plot of Oxygen and depth 2018-02-17
-par(mfcol = c(2, 1),  mar = c(2, 10, 2, 2))
-plot(ODO ~ time_point, data = YSI, subset = Date >= "2018-02-17" & Date <= "2018-02-17" , type= "p", ylab = "Dissolved Oxygen (mg/L)", main = "2018-02-17" , col= "blue", pch = 1, axes = F, ylim = c(5, 15))
+#### Plot of Oxygen and depth spring
+par(mfcol = c(2, 1),  mar = c(0, 5, 2, 5))
+plot(ODO ~ time_point, data = YSI, subset = Date < "2018-04-09" & Date > "2018-03-15" , type= "l", ylab = "Dissolved Oxygen (mg/L)", main = "Spring" , col= "blue", pch = 1, axes = F, ylim = c(5, 15))
 axis(2)
-#axis(1, c("00:30:00", "05:15:00", "10:15:00", "15:15:00"), at = c(10240, 10260, 10280, 10300))
 box()
-plot(Pressure ~ Date.Time, data = pres, subset = Date.Time < "2018-02-17 24:59:59" & Date.Time > "2018-02-16 24:59:59")
+par(mar = c(2, 5, 0, 5))
+plot(Value ~ Date.Time, data = pres, subset = Date.Time < "2018-04-08 24:59:59" & Date.Time > "2018-03-15 24:59:59", type = "l", ylab = "Depth (cm)")
 
-#### Plot of Oxygen 
-par(mfcol = c(2, 1),  mar = c(2, 10, 2, 2))
-plot(ODO ~ time_point, data = YSI, subset = Date < "2018-04-14" & Date > "2018-04-01" , type= "p", ylab = "ODO (mg/L)", main = "2018-02-17" , col= "blue", pch = 1, axes = F, ylim = c(5, 15))
-axis(2)
-#axis(1, c("00:30:00", "05:15:00", "10:15:00", "15:15:00"), at = c(10240, 10260, 10280, 10300))
-box()
-plot(Pressure ~ Date.Time, data = pres, subset = Date.Time < "2018-04-14 24:59:59" & Date.Time > "2018-04-01
-     24:59:59")
 
-####plots
-
-par(mfcol = c(2, 1),  mar = c(2, 10, 2, 2))
-plot(FDOM ~ SAL, data = YSI, subset = Date < "2018-02-20" & Date > "2017-11-10" , type= "l", ylab = "TURB", main = "Turbidity"  ,col= "blue", pch = 1, axes = F )
-axis(2)
-#axis(1, c("00:30:00", "05:15:00", "10:15:00", "15:15:00"), at = c(10240, 10260, 10280, 10300))
-box()
-plot(FDOM ~ SAL, data = YSI, subset = Date.Time < "2018-02-20 24:59:59" & Date.Time > "2017-11-10 24:59:59" , type= "l")  par(las = 1, mfcol = c(1, 1), mar = c(5, 5, 5, 5)) plot(FDOM ~ SAL, data = YSI, subset = Date < "2017-12-05" & Date > "2017-11-05" , type= "p", ylab = "FDOM", main = "Salinity and FDOM November-December 2017"  ,col= "red", pch = 1, axes = T)  boxplot(YSI$ODO[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$ODO[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$ODO[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F, ylim = c(0, 20)) axis(2) axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3)) box()
-
-## Plots by Season 
+## Plots by Season
+### FDOM by Season
 boxplot(YSI$FDOM[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$FDOM[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$FDOM[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F)
 axis(2)
 axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
 
+### ODO by Season 
+#### All Data
+boxplot(YSI$ODO[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$ODO[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$ODO[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F)
+axis(2)
+axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
+
+#### By Daily Mean
+sp_ODO_day_mean <- as.numeric(tapply(YSI$ODO[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$Date[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], mean))
+winter_ODO_day_mean <- as.numeric(tapply(YSI$ODO[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$Date[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], mean))
+fall_ODO_day_mean <- as.numeric(tapply(YSI$ODO[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], YSI$Date[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], mean))
+
+##### Plot
+boxplot(sp_ODO_day_mean, winter_ODO_day_mean, fall_ODO_day_mean, axes = F)
+axis(2)
+axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
+
+##### Combined Plot
+par(las = 1, mfcol = c(2, 1), mar = c(0, 5, 3, 5)) 
+boxplot(YSI$ODO[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$ODO[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$ODO[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F, ylim = c(3, 20))
+axis(2)
+box()
+par(mar = c(3, 5, 0, 5))
+boxplot(sp_ODO_day_mean, winter_ODO_day_mean, fall_ODO_day_mean, axes = F, ylim = c(3, 20))
+axis(2)
+axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
+box()
+
+### SAL by Season
 boxplot(YSI$SAL[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$SAL[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$SAL[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F) 
 axis(2)
 axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
 
+### TURB by Season
 boxplot(YSI$TURB[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$TURB[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$TURB[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F) 
 axis(2)
 axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
 
+### pH by Season 
 boxplot(YSI$PH[YSI$Date < "2018-04-14" & YSI$Date > "2018-03-15"], YSI$PH[YSI$Date < "2018-03-15" & YSI$Date > "2017-12-15"], YSI$PH[YSI$Date < "2017-12-15" & YSI$Date > "2017-11-11"], axes = F) 
 axis(2)
 axis(1, c("Spring", "Winter", "Fall"), at = c(1, 2, 3))
 
-# Correlation by Season
+# Correlation with Salinity by Season
+    par(las = 1)
     plot(FDOM ~ SAL, data = YSI, subset = Date < "2018-04-15" & Date > "2018-03-15", xlim = c(0, 20), pch = ".")
     points(FDOM ~ SAL, data = YSI, subset = Date < "2018-03-15" & Date > "2017-12-15", col = 2, pch = ".")
     points(FDOM ~ SAL, data = YSI, subset = Date < "2017-12-15" & Date > "2017-11-11", col = 3, pch = ".")
     legend(0, 10, c("Spring", "Winter", "Fall"), pch = 19, col = c(1, 2, 3))
     
-    plot(ODO ~ SAL, data = YSI, subset = Date < "2018-04-15" & Date > "2018-03-15", xlim = c(0, 20), ylim = c(0, 20))
-    points(ODO ~ SAL, data = YSI, subset = Date < "2018-03-15" & Date > "2017-12-15", col = 2)
-    points(ODO ~ SAL, data = YSI, subset = Date < "2017-12-15" & Date > "2017-11-11", col = 3)
+    plot(ODO ~ SAL, data = YSI, subset = Date < "2018-04-15" & Date > "2018-03-15", xlim = c(0, 20), ylim = c(0, 20), pch = ".")
+    points(ODO ~ SAL, data = YSI, subset = Date < "2018-03-15" & Date > "2017-12-15", col = 2, pch = ".")
+    points(ODO ~ SAL, data = YSI, subset = Date < "2017-12-15" & Date > "2017-11-11", col = 3, pch = ".")
     
-    plot(PH ~ SAL, data = YSI, subset = Date < "2018-04-15" & Date > "2018-03-15", xlim = c(0, 20), ylim = c(5, 10))
-    points(PH ~ SAL, data = YSI, subset = Date < "2018-03-15" & Date > "2017-12-15", col = 2)
-    points(PH ~ SAL, data = YSI, subset = Date < "2017-12-15" & Date > "2017-11-11", col = 3)
+    plot(PH ~ SAL, data = YSI, subset = Date < "2018-04-15" & Date > "2018-03-15", xlim = c(0, 20), ylim = c(7, 9), pch = ".")
+    points(PH ~ SAL, data = YSI, subset = Date < "2018-03-15" & Date > "2017-12-15", col = 2, pch = ".")
+    points(PH ~ SAL, data = YSI, subset = Date < "2017-12-15" & Date > "2017-11-11", col = 3, pch = ".")
     
     plot(TURB ~ SAL, data = YSI, subset = Date < "2018-04-15" & Date > "2018-03-15", xlim = c(0, 20), ylim = c(0, 30), pch = ".")
     points(TURB ~ SAL, data = YSI, subset = Date < "2018-03-15" & Date > "2017-12-15", col = 2, pch = ".")
     points(TURB ~ SAL, data = YSI, subset = Date < "2017-12-15" & Date > "2017-11-11", col = 3, pch = ".")
+    
     
