ad03c7ca166afcb48c414656850cbca422076887
Kenneth Fortino <fortinok@longwood.edu>
2019-10-02 20:45:55 -0400

    updated HSF weather cleaning script and cleaned HSF weather file

--- lab_notebook/analysis/HSF_weather_datafile_cleaning.R
+++ lab_notebook/analysis/HSF_weather_datafile_cleaning.R
@@ -1,25 +1,25 @@
-# Cleaning Script for .dat files that are exported from the LP Weather Station
+# Cleaning Script for .dat files that are exported from the HSF Weather Station
 
 ## Enter User Data
 ### Input filenames
 
-   input.T1.raw.file <- "./data/LP_weather_2019-08-28_T1_raw.dat"
-   input.T2.raw.file <- "./data/LP_weather_2019-08-28_T2_raw.dat"
+   input.T1.raw.file <- "./data/CR200Series_HSF_Weather_Table1.dat"
+   input.T2.raw.file <- "./data/CR200Series_HSF_Weather_Table2.dat"
    
-   output.T1.data.file <- "./data/LP_weather_2019-08-28_T1_data.csv" 
-   output.T2.data.file <- "./data/LP_weather_2019-08-28_T2_data.csv" 
+   output.T1.data.file <- "./data/HSF_weather_2019-09-29_T1_data.csv" 
+   output.T2.data.file <- "./data/HSF_weather_2019-09-29_T2_data.csv" 
    
-   output.clean.file <- "./data/LP_weather_2019-08-28.csv" 
+   output.clean.file <- "./data/HSF_weather_2019-09-28.csv" 
     # file name format = ./data/site_date.csv
 
-   metadata.file <- "./metadata/LP_weather_2019-08-28_metadata.txt" 
+   metadata.file <- "./data/metadata/HSF_weather_2019-09-29_metadata.txt" 
     # file name format = ./data/site_date.csv
 
 
 ## Import the data file as text
    
-   LP.weather.T1.raw <- readLines(input.T1.raw.file, skipNul = T)
-   LP.weather.T2.raw <- readLines(input.T2.raw.file, skipNul = T)
+   HSF.weather.T1.raw <- readLines(input.T1.raw.file, skipNul = T)
+   HSF.weather.T2.raw <- readLines(input.T2.raw.file, skipNul = T)
 
 ### Notes
 
@@ -28,64 +28,64 @@ At this point the data file contains 5 lines of metadata before the data begin a
 ## Extract metadata
 ### Remove the header information from the metadata
 
-    LP.weather.T1.header <- LP.weather.T1.raw[2]
-    LP.weather.T2.header <- LP.weather.T2.raw[2]
+    HSF.weather.T1.header <- HSF.weather.T1.raw[2]
+    HSF.weather.T2.header <- HSF.weather.T2.raw[2]
 
 ### Extract the total metadata
     
-    LP.weather.T1.metadata <- LP.weather.T1.raw[c(1:4)]
-    LP.weather.T2.metadata <- LP.weather.T2.raw[c(1:4)]
+    HSF.weather.T1.metadata <- HSF.weather.T1.raw[c(1:4)]
+    HSF.weather.T2.metadata <- HSF.weather.T2.raw[c(1:4)]
 
 ### Remove the metadata from the file
     
-    LP.weather.T1.wo.head <- LP.weather.T1.raw[-c(1:4) ]
-    LP.weather.T2.wo.head <- LP.weather.T2.raw[-c(1:4) ]
+    HSF.weather.T1.wo.head <- HSF.weather.T1.raw[-c(1:4) ]
+    HSF.weather.T2.wo.head <- HSF.weather.T2.raw[-c(1:4) ]
     
 ## Add header information to the data file
     
-    LP.weather.T1 <- c(LP.weather.T1.header, LP.weather.T1.wo.head)
-    LP.weather.T2 <- c(LP.weather.T2.header, LP.weather.T2.wo.head)
+    HSF.weather.T1 <- c(HSF.weather.T1.header, HSF.weather.T1.wo.head)
+    HSF.weather.T2 <- c(HSF.weather.T2.header, HSF.weather.T2.wo.head)
 
 ## Write to csv file
 
-    writeLines(LP.weather.T1, con = output.T1.data.file) 
-    writeLines(LP.weather.T2, con = output.T2.data.file) 
+    writeLines(HSF.weather.T1, con = output.T1.data.file) 
+    writeLines(HSF.weather.T2, con = output.T2.data.file) 
 
 ## Import raw data-only csv file as a data.frame
     
-    LP.weather.T1 <- read.table(output.T1.data.file, header = T, sep = ",")
-    LP.weather.T2 <- read.table(output.T2.data.file, header = T, sep = ",")
+    HSF.weather.T1 <- read.table(output.T1.data.file, header = T, sep = ",")
+    HSF.weather.T2 <- read.table(output.T2.data.file, header = T, sep = ",")
     
 ## Merge T1 and T2   
 
-   LP.weather <- merge(LP.weather.T1, LP.weather.T2, by = "TIMESTAMP")
+   HSF.weather <- merge(HSF.weather.T1, HSF.weather.T2, by = "TIMESTAMP")
    
 ## Clean up TIMESTAMP
 ### Convert TIMESTAMP to POSIXct
 
-    LP.weather$TIMESTAMP <- as.POSIXct(LP.weather$TIMESTAMP)
+    HSF.weather$TIMESTAMP <- as.POSIXct(HSF.weather$TIMESTAMP)
     
 ### Create DATE variable
     
-    DATE <- as.Date(strptime(LP.weather$TIMESTAMP, format = "%Y-%m-%d"))
+    DATE <- as.Date(strptime(HSF.weather$TIMESTAMP, format = "%Y-%m-%d"))
 
-### Add DATE to LP.weather data.frame
+### Add DATE to HSF.weather data.frame
     
-    LP.weather <- data.frame(DATE, LP.weather)
+    HSF.weather <- data.frame(DATE, HSF.weather)
 
 ## Write new data file of cleaned data
 
-    write.table(LP.weather, file = output.clean.file, row.names = F, quote = F, sep = ",")
+    write.table(HSF.weather, file = output.clean.file, row.names = F, quote = F, sep = ",")
 
 ## Generate Metadata
     
 ### Combine T1 and T2 metadata files
     
-    LP.weather.metadata <- c(LP.weather.T1.metadata, LP.weather.T2.metadata)
+    HSF.weather.metadata <- c(HSF.weather.T1.metadata, HSF.weather.T2.metadata)
 
 ### Write to metadata text file
    
-    writeLines(LP.weather.metadata, con = metadata.file) 
+    writeLines(HSF.weather.metadata, con = metadata.file) 
     
 ### Remove temp files
     
