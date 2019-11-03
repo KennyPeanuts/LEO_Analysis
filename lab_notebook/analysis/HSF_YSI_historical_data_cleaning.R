92b80cea8c5c79fa79caf6449aafcc8c95308b4e
Kenneth Fortino <fortinok@longwood.edu>
2019-11-03 14:49:09 -0500

    cleaned historical YSI data

--- lab_notebook/analysis/HSF_YSI_historical_data_cleaning.R
+++ lab_notebook/analysis/HSF_YSI_historical_data_cleaning.R
@@ -6,17 +6,29 @@ The data were combined into a single .csv file from the KOR software by DML
 
 ## Import Data
 
-    YSI <- read.table("data/HSF_YSI_2015-2018.csv", header = T, sep = ",", skipNul = T, encoding = "latin1")
+    YSI <- read.table("./data/HSF_YSI_2015-2018.csv", header = T, sep = ",", skipNul = T, encoding = "latin1")
     
     date.time <- paste(as.character(YSI$Date), as.character(YSI$Time))
-   
+    
+### Fix the weird character in Date
+    
+    names(YSI)[1] <- "Date"
+    
 ## Convert date to YYYY-MM-DD HH:MM:SS
     
-    #date <- strptime(as.character(date), format = "%m/%d/%Y") # this may not work
-    date.time <- strptime(as.character(date.time), format = "%m/%d/%y %H:%M:%S") # produces POSIXlt, which is a list
-    DateTime <- as.POSIXct(date.time) # converts to POSIXct, which can be used as a variable
+    Date <- strptime(as.character(YSI$Date), format = "%m/%d/%Y")
+    Time <- strptime(as.character(YSI$Time), format = "%H:%M:%S")
+    DateTime <- c(Date, Time)
 
 ## Add the DateTime Variable Back to the YSI data.frame
     
     YSI <- data.frame(DateTime, YSI)
+    
+## Remove variable X at end of the data.frame
+    
+    YSI <- YSI[-32]
+    
+## Write to file
+    
+    write.table(YSI, "./data/HSF_YSI_2015-2018.csv", quote = F, row.names = F, sep = ",")
     
